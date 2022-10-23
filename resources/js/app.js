d5d4d4dfd1ab45a26fab0990a6e4582dcdb407d4
MichalPedrak <m.pedrak@runbyit.com>
2022-10-23 21:01:45 +0200

    start of the project

---
+++ resources/js/app.js
@@ -0,0 +1,41 @@
+import './bootstrap';
+
+import Alpine from 'alpinejs';
+
+import '../../public/assets/css/style.css'
+
+import '../../public/assets/images/brand/favicon.ico'
+
+import '../../public/assets/css/transparent-style.css'
+
+import '../../public/assets/css/skin-modes.css'
+
+import '../../public/assets/css/icons.css'
+
+import '../../public/assets/colors/color1.css'
+
+
+
+
+
+
+// import '../../public/assets/plugins/bootstrap/js/popper.min.js'
+// import '../../public/assets/plugins/input-mask/jquery.mask.min.js'
+// import '../../public/assets/plugins/sidemenu/sidemenu.js'
+// import '../../public/assets/plugins/sidebar/sidebar.js'
+// import '../../public/assets/plugins/bootstrap5-typehead/autocomplete.js'
+// import '../../public/assets/plugins/select2/select2.full.min.js'
+// import '../../public/assets/js/select2.js'
+// import '../../public/assets/plugins/p-scroll/perfect-scrollbar.js'
+// import '../../public/assets/plugins/p-scroll/pscroll.js'
+// import '../../public/assets/plugins/p-scroll/pscroll-1.js'
+// import '../../public/assets/js/typehead.js'
+// import '../../public/assets/js/sticky.js'
+// import '../../public/assets/js/custom.js'
+
+
+
+
+window.Alpine = Alpine;
+
+Alpine.start();
