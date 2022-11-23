38e3b5bbaedb60ad8ed2fb4f01574db75792f51a
MichalPedrak <michalpedrak07@gmail.com>
2022-11-23 21:30:05 +0100

    Add login and register page from template

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,57 +1,153 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+<!doctype html>
+<html lang="en" dir="ltr">
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+<head>
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+    <!-- META DATA -->
+    <meta charset="UTF-8">
+    <meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Sash – Bootstrap 5  Admin & Dashboard Template">
+    <meta name="author" content="Spruko Technologies Private Limited">
+    <meta name="keywords" content="admin,admin dashboard,admin panel,admin template,bootstrap,clean,dashboard,flat,jquery,modern,responsive,premium admin templates,responsive admin,ui,ui kit.">
 
-            <!-- Email Address -->
-            <div>
-                <x-input-label for="email" :value="__('Email')" />
+    <!-- FAVICON -->
+    <link rel="shortcut icon" type="image/x-icon" href="../assets/images/brand/favicon.ico" />
 
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+    <!-- TITLE -->
+    <title>Sash – Bootstrap 5 Admin & Dashboard Template</title>
 
-                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    <!-- BOOTSTRAP CSS -->
+    <link id="style" href="../assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+
+    <!-- STYLE CSS -->
+    <link href="../assets/css/style.css" rel="stylesheet" />
+    <link href="../assets/css/dark-style.css" rel="stylesheet" />
+    <link href="../assets/css/transparent-style.css" rel="stylesheet">
+    <link href="../assets/css/skin-modes.css" rel="stylesheet" />
+
+    <!--- FONT-ICONS CSS -->
+    <link href="../assets/css/icons.css" rel="stylesheet" />
+
+    <!-- COLOR SKIN CSS -->
+    <link id="theme" rel="stylesheet" type="text/css" media="all" href="../assets/colors/color1.css" />
+
+</head>
+
+<body class="app sidebar-mini ltr login-img">
+<!-- todo: blade componenets and errors -->
+<!-- BACKGROUND-IMAGE -->
+<div class="">
+
+    <!-- GLOABAL LOADER -->
+    <div id="global-loader">
+        <img src="../assets/images/loader.svg" class="loader-img" alt="Loader">
+    </div>
+    <!-- /GLOABAL LOADER -->
+
+    <!-- PAGE -->
+    <div class="page">
+        <div class="">
+
+            <!-- CONTAINER OPEN -->
+            <div class="col col-login mx-auto mt-7">
+                <div class="text-center">
+                    <a href="index.html"><img src="../assets/images/brand/logo-white.png" class="header-brand-img" alt=""></a>
+                </div>
             </div>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-input-label for="password" :value="__('Password')" />
+            <div class="container-login100">
+                <div class="wrap-login100 p-6">
+{{--                    <form class="login100-form validate-form">--}}
+                            <span class="login100-form-title pb-5">
+                                Login
+                            </span>
+                        <div class="panel panel-primary">
+                            <div class="tab-menu-heading">
+                                <div class="tabs-menu1">
+                                    <!-- Tabs -->
+                                    <ul class="nav panel-tabs">
+                                        <li class="mx-0"><a href="#tab5" class="active" data-bs-toggle="tab">Email</a></li>
+{{--                                        <li class="mx-0"><a href="#tab6" data-bs-toggle="tab">Mobile</a></li>--}}
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="panel-body tabs-menu-body p-0 pt-5">
+                                <div class="tab-content">
+                                    <div class="tab-pane active" id="tab5">
+                                        <form method="POST" action="{{ route('login') }}">
+                                            @csrf
+                                            <!-- EMAIL -->
+                                            <div class="wrap-input100 validate-input input-group" data-bs-validate="Valid email is required: ex@abc.xyz">
+                                                <a href="javascript:void(0)" class="input-group-text bg-white text-muted">
+                                                    <i class="zmdi zmdi-email text-muted" aria-hidden="true"></i>
+                                                </a>
+                                                <input :value="old('email')" required autofocus class="input100 border-start-0 form-control ms-0" type="email" placeholder="Email" name="email">
+                                            </div>
+                                            <!-- PASSWORD -->
+                                            <div class="wrap-input100 validate-input input-group" id="Password-toggle">
+                                                <a href="javascript:void(0)" class="input-group-text bg-white text-muted">
+                                                    <i class="zmdi zmdi-eye text-muted" aria-hidden="true"></i>
+                                                </a>
+                                                <input required autofocus class="input100 border-start-0 form-control ms-0" type="password" placeholder="Password" name="password">
+                                            </div>
+                                            <!-- FORGOT PASSWORD-->
+                                            @if (Route::has('password.request'))
+                                                <div class="text-end pt-4">
+                                                    <p class="mb-0"><a href="{{ route('password.request') }}" class="text-primary ms-1">{{ __('Forgot your password?') }}</a></p>
+                                                </div>
+                                            @endif
+                                            <!-- LOGIN -->
+                                            <div class="container-login100-form-btn">
+                                                <button type="submit" class="login100-form-btn btn-primary">
+                                                    {{ __('Log in') }}
+                                                </button>
+                                            </div>
+                                            <!-- REGISTER -->
+                                            <div class="text-center pt-3">
+                                                <p class="text-dark mb-0">Not a member?<a href="/register" class="text-primary ms-1">Sign UP</a></p>
+                                            </div>
+                                        </form>
 
-                <x-text-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
-                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+{{--                    </form>--}}
+                </div>
             </div>
+            <!-- CONTAINER CLOSED -->
+        </div>
+    </div>
+    <!-- End PAGE -->
 
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
+</div>
+<!-- BACKGROUND-IMAGE CLOSED -->
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
+<!-- JQUERY JS -->
+<script src="../assets/js/jquery.min.js"></script>
 
-                <x-primary-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-primary-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+<!-- BOOTSTRAP JS -->
+<script src="../assets/plugins/bootstrap/js/popper.min.js"></script>
+<script src="../assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+
+<!-- SHOW PASSWORD JS -->
+<script src="../assets/js/show-password.min.js"></script>
+
+<!-- GENERATE OTP JS -->
+<script src="../assets/js/generate-otp.js"></script>
+
+<!-- Perfect SCROLLBAR JS-->
+<script src="../assets/plugins/p-scroll/perfect-scrollbar.js"></script>
+
+<!-- Color Theme js -->
+<script src="../assets/js/themeColors.js"></script>
+
+<!-- CUSTOM JS -->
+<script src="../assets/js/custom.js"></script>
+
+
+</body>
+
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,65 +1,137 @@
-<x-guest-layout>
-    <x-auth-card>
-        <!-- Logo -->
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+<!doctype html>
+<html lang="en" dir="ltr">
 
-        <form method="POST" action="{{ route('register') }}"> <!-- Redirectuje nas do POST, tego samego routa -->
-            @csrf
+<head>
 
-            <!-- Name -->
-            <div>
-                <x-input-label for="name" :value="__('Name')" />
+    <!-- META DATA -->
+    <meta charset="UTF-8">
+    <meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Sash – Bootstrap 5  Admin & Dashboard Template">
+    <meta name="author" content="Spruko Technologies Private Limited">
+    <meta name="keywords" content="admin,admin dashboard,admin panel,admin template,bootstrap,clean,dashboard,flat,jquery,modern,responsive,premium admin templates,responsive admin,ui,ui kit.">
 
-                <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+    <!-- FAVICON -->
+    <link rel="shortcut icon" type="image/x-icon" href="../assets/images/brand/favicon.ico" />
 
-                <x-input-error :messages="$errors->get('name')" class="mt-2" />
+    <!-- TITLE -->
+    <title>Sash – Bootstrap 5 Admin & Dashboard Template</title>
+
+    <!-- BOOTSTRAP CSS -->
+    <link id="style" href="../assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+
+    <!-- STYLE CSS -->
+    <link href="../assets/css/style.css" rel="stylesheet" />
+    <link href="../assets/css/dark-style.css" rel="stylesheet" />
+    <link href="../assets/css/transparent-style.css" rel="stylesheet">
+    <link href="../assets/css/skin-modes.css" rel="stylesheet" />
+
+    <!--- FONT-ICONS CSS -->
+    <link href="../assets/css/icons.css" rel="stylesheet" />
+
+    <!-- COLOR SKIN CSS -->
+    <link id="theme" rel="stylesheet" type="text/css" media="all" href="../assets/colors/color1.css" />
+
+</head>
+
+<body class="app sidebar-mini ltr login-img">
+<!-- todo: blade componenets and errors -->
+<!-- BACKGROUND-IMAGE -->
+<div class="">
+
+    <!-- GLOABAL LOADER -->
+    <div id="global-loader">
+        <img src="../assets/images/loader.svg" class="loader-img" alt="Loader">
+    </div>
+    <!-- /GLOABAL LOADER -->
+
+    <!-- PAGE -->
+    <div class="page">
+        <div class="">
+
+            <!-- CONTAINER OPEN -->
+            <div class="col col-login mx-auto mt-7">
+                <div class="text-center">
+                    <a href="index.html"><img src="../assets/images/brand/logo-white.png" class="header-brand-img m-0" alt=""></a>
+                </div>
             </div>
+            <div class="container-login100">
+                <div class="wrap-login100 p-6">
+                    <form method="POST" action="{{ route('register') }}"> <!-- Redirectuje nas do POST, tego samego routa -->
+                        @csrf
+                            <span class="login100-form-title">
+									Registration
+								</span>
+                        <div class="wrap-input100 validate-input input-group" data-bs-validate="Valid email is required: ex@abc.xyz">
+                            <a href="javascript:void(0)" class="input-group-text bg-white text-muted">
+                                <i class="mdi mdi-account" aria-hidden="true"></i>
+                            </a>
+                            <input class="input100 border-start-0 ms-0 form-control" type="text" placeholder="Name" name="name" :value="old('name')" required autofocus >
+                        </div>
+                        <div class="wrap-input100 validate-input input-group" data-bs-validate="Valid email is required: ex@abc.xyz">
+                            <a href="javascript:void(0)" class="input-group-text bg-white text-muted">
+                                <i class="zmdi zmdi-email" aria-hidden="true"></i>
+                            </a>
+                            <input class="input100 border-start-0 ms-0 form-control" type="email" placeholder="Email"  name="email" :value="old('email')" required >
+                        </div>
+                        <div class="wrap-input100 validate-input input-group" id="Password-toggle">
+                            <a href="javascript:void(0)" class="input-group-text bg-white text-muted">
+                                <i class="zmdi zmdi-eye" aria-hidden="true"></i>
+                            </a>
+                            <input class="input100 border-start-0 ms-0 form-control" type="password" placeholder="Password" name="password">
+                        </div>
+                        <div class="wrap-input100 validate-input input-group" id="Password-toggle">
+                            <a href="javascript:void(0)" class="input-group-text bg-white text-muted">
+                                <i class="zmdi zmdi-eye" aria-hidden="true"></i>
+                            </a>
+                            <input class="input100 border-start-0 ms-0 form-control" type="password" placeholder="Password" name="password_confirmation">
+                        </div>
+                        <label class="custom-control custom-checkbox mt-4">
+                            <input type="checkbox" class="custom-control-input">
+                            <span class="custom-control-label">Agree the <a href="terms.html">terms and policy</a></span>
+                        </label>
+                        <div class="container-login100-form-btn">
+                            <button type="submit" href="index.html" class="login100-form-btn btn-primary">
+                                {{ __('Register') }}
+                            </button>
+                        </div>
+                        <div class="text-center pt-3">
+                            <p class="text-dark mb-0">Already have account?<a href="{{ route('login') }}" class="text-primary ms-1">Sign In</a></p>
+                        </div>
 
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-input-label for="email" :value="__('Email')" />
 
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                    </form>
 
-                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                </div>
             </div>
+            <!-- CONTAINER CLOSED -->
+        </div>
+    </div>
+    <!-- END PAGE -->
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-input-label for="password" :value="__('Password')" />
+</div>
+<!-- BACKGROUND-IMAGE CLOSED -->
 
-                <x-text-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
+<!-- JQUERY JS -->
+<script src="../assets/js/jquery.min.js"></script>
 
-                <x-input-error :messages="$errors->get('password')" class="mt-2" />
-            </div>
+<!-- BOOTSTRAP JS -->
+<script src="../assets/plugins/bootstrap/js/popper.min.js"></script>
+<script src="../assets/plugins/bootstrap/js/bootstrap.min.js"></script>
 
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+<!-- SHOW PASSWORD JS -->
+<script src="../assets/js/show-password.min.js"></script>
 
-                <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
+<!-- Perfect SCROLLBAR JS-->
+<script src="../assets/plugins/p-scroll/perfect-scrollbar.js"></script>
 
-                <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-            </div>
+<!-- Color Theme js -->
+<script src="../assets/js/themeColors.js"></script>
 
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
+<!-- CUSTOM JS -->
+<script src="../assets/js/custom.js"></script>
 
-                <x-primary-button class="ml-4">
-                    {{ __('Register') }}
-                </x-primary-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+
+</body>
+
+</html>
